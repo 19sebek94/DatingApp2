e40bf63c3cfa8079338f769a1e6e9e26a3159fdd
Sebastian Turek <sebastiant1994@users.noreply.github.com>
2019-07-28 10:42:01 +0200

    End of section 3 and 4

--- DatingApp2.API/Controllers/AuthController.cs
+++ DatingApp2.API/Controllers/AuthController.cs
@@ -2,9 +2,14 @@
 using DatingApp2.API.Dtos;
 using DatingApp2.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace DatingApp2.API.Controllers
@@ -14,8 +19,11 @@ namespace DatingApp2.API.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _repo;
-        public AuthController(IAuthRepository repo)
+        private readonly IConfiguration _config;
+
+        public AuthController(IAuthRepository repo, IConfiguration config)
         {
+            _config = config;
             _repo = repo;
         }
 
@@ -34,6 +42,41 @@ namespace DatingApp2.API.Controllers
             var createdUser = await _repo.Register(userToCreate, userForRegister.Password);
 
             return StatusCode(201);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+            var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
+            if (userFromRepo == null)
+                return Unauthorized();
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.Username)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8
+                .GetBytes(_config.GetSection("AppSettings:Token").Value)); // generuje klucz
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);//hashuje klucz
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            }; //zawiera info o tokenie
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor); //tworzy token
+            //zawiera jwt token przesyłane do klienta
+            return Ok(new
+            {
+                token = tokenHandler.WriteToken(token)
+            });
         }
     }
 }
--- DatingApp2.API/Dtos/UserForLoginDto.cs
+++ DatingApp2.API/Dtos/UserForLoginDto.cs
@@ -1,17 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace DatingApp2.API.Dtos
 {
-    public class UserForRegister
+    public class UserForLoginDto
     {
-        [Required]
         public string Username { get; set; }
-        [Required]
-        [StringLength(8, MinimumLength = 4, ErrorMessage = "You must specify password between 4 and 8 chars")]
         public string Password { get; set; }
     }
 }
